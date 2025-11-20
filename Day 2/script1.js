a49b7790ddfb4ab221010c9955d4b922b65ab1f0
Shreya Jadhav <142742335+Shreyaa-J@users.noreply.github.com>
2025-11-20 12:41:58 +0530

    Add script1.js to demonstrate variable types

---
+++ Day 2/script1.js
@@ -0,0 +1,20 @@
+let firstName="Shree";
+console.log(typeof firstName) 
+
+let id=0.23
+console.log(typeof id)
+
+let marks;
+console.log(typeof marks)   //undefined
+
+let age=null;
+console.log(typeof age)    //object
+
+let isfollow= true;
+console.log(typeof isfollow)
+
+let contact= 9876543210n;
+console.log(typeof contact) //n denotes big int here
+
+let contact2= BigInt(9876543210);
+console.log(typeof contact2)// here bigint function is used to convert number to big int
